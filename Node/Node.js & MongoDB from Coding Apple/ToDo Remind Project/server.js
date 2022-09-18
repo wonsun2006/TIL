fd77af730af5eded912e13136c4daabb81ffec10
wonsun2006 <44808210+wonsun2006@users.noreply.github.com>
2022-09-18 23:49:18 +0900

    [22.09.18] write, delete 추가
    1. write 기능 추가 완료
    2. delete 기능 추가 미완료

--- Node/Node.js & MongoDB from Coding Apple/ToDo Remind Project/server.js
+++ Node/Node.js & MongoDB from Coding Apple/ToDo Remind Project/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 require('dotenv').config();
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.set('view engine', 'ejs');
+app.use(methodOverride('_method'));
 
 let db;
 
@@ -17,8 +23,6 @@ MongoClient.connect(process.env.DB_URL, (error, client)=>{
     
 });
 
-app.set('view engine', 'ejs');
-
 app.get('/', (req, res)=>{
     res.render('index.ejs');
 });
@@ -32,3 +36,43 @@ app.get('/list', (req, res)=>{
 app.get('/write-form', (req, res)=>{
     res.render('write-form.ejs');
 }); 
+
+app.post('/write', (req, res)=>{
+    db.collection('counter').findOne({name: "게시물갯수"}, (error, result)=>{
+        if(error){
+            res.send('DB Data Error');
+        }
+
+        todoData = {
+            _id: result.totalPost+1,
+            제목: req.body.title,
+            작성자: req.body.writer,
+            날짜: req.body.date
+        }
+
+        db.collection('post').insertOne(todoData, (error, result)=>{
+            if(error){
+                res.send('DB Data Error');
+            }
+
+            db.collection('counter').updateOne({name: "게시물갯수"},{$inc : {totalPost:1}},(error, result)=>{
+                if(error){
+                    res.send('DB Data Error');
+                }
+                res.redirect('/list');
+            });
+        });
+    });
+});
+
+app.delete('/delete', (req, res)=>{
+    const idToDelete = parseInt(req.body._id);
+
+    db.collection('post').deleteOne({_id : idToDelete},(error, result)=>{
+        if(error){
+            alert('failed to delete todo');
+        }else{
+            res.status(200).send({message : '삭제 성공'});
+        }
+    });
+})
